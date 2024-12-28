e4ea0ba18535c554bd083c6cca3d1bcb81f497c0
Akshay Kishor Chauhan <akshaykishorchauhan@gmail.com>
2024-12-28 22:23:49 +0530

    Added Episode 4
    Added Episode 4: Styling SPFx Web Parts with Fluent UI

---
+++ Episode 4 Styling SPFx Web Parts with Fluent UI/src/webparts/helloWorld/components/HelloWorld.module.scss.ts
@@ -0,0 +1,11 @@
+
+require("./HelloWorld.module.css");
+const styles = {
+  helloWorld: 'helloWorld_46f62f73',
+  teams: 'teams_46f62f73',
+  welcome: 'welcome_46f62f73',
+  welcomeImage: 'welcomeImage_46f62f73',
+  links: 'links_46f62f73'
+};
+
+export default styles;
